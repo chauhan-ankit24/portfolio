2ace6113fc04386389bbd747cce6bac64deeecfd
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-09-18 09:24:35 +0530

    working

---
+++ pages/projects/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+const Projects = () => {
+  return <div>
+    <Head>
+      <title>Projects | Portfolio</title>
+      <link rel="icon" href="https://img.icons8.com/?size=100&id=19302&format=png&color=1A1A1A" />
+    </Head>
+    Projects</div>;
+};
+
+export default Projects;
