4b8b3061d248102ade72a36df8e68a7e55e5c982
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-09-18 09:24:02 +0530

    working

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -1,11 +1,10 @@
-import Head from "next/head";
+import Head from "next/head"
 
 const Experience = () => {
   return <div>
     <Head>
       <title>Experience | Portfolio</title>
-      <link rel="icon" href="https://img.icons8.com/?size=100&id=kbNSTR9zOMUk&format=png&color=1A1A1A
-" />
+      <link rel="icon" href="https://img.icons8.com/?size=100&id=kbNSTR9zOMUk&format=png&color=1A1A1A" />
     </Head>
     Experience</div>;
 };
